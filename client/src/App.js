94eb11099b2cdbb86fe74e64d63d5a4dc99e7b8e
Arsalan Sadri <asadri62@gmail.com>
2018-10-27 20:28:06 -0700

    adding a basic navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import Navbar from "./components/Navbar";
 
 const App = () => (
   <Router>
-    <div>
-      <Nav />
-      <Switch>
+    <div className="container">
+      <Navbar />
+      {/* <Switch>
         <Route exact path="/" component={Books} />
         <Route exact path="/books" component={Books} />
         <Route exact path="/books/:id" component={Detail} />
         <Route component={NoMatch} />
-      </Switch>
+      </Switch> */}
     </div>
   </Router>
 );
