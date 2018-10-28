3369873567c5052ee593128e2cf6ced30078b0e0
Arsalan Sadri <asadri62@gmail.com>
2018-10-28 01:15:50 -0700

    adding a basic submit form

--- client/src/components/SignUp/SignUp.js
+++ client/src/components/SignUp/SignUp.js
@@ -1,8 +1,55 @@
 import React from "react";
 import "./SignUp.css";
 
-const SignUp = () => (
-    <div>This is a sign-up form!!!</div>
-);
+class SignUp extends React.Component {
+    state = {
+        firstName: "",
+        lastName: ""
+    };
+
+    inputChangeHandler = event => {
+        const { name, value } = event.target;
+
+        this.setState({
+            [name]: value
+        });
+    };
+
+    formSubmitHandler = event => {
+        event.preventDefault();
+        alert(`Hello ${this.state.firstName} ${this.state.lastName}`);
+        this.setState({
+            firstName: "",
+            lastName: ""
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <p>
+                    Hello {this.state.firstName} {this.state.lastName}
+                </p>
+                <form className="form">
+                    <input
+                        value={this.state.firstName}
+                        name="firstName"
+                        onChange={this.inputChangeHandler}
+                        type="text"
+                        placeholder="First Name"
+                    />
+                    <input
+                        value={this.state.lastName}
+                        name="lastName"
+                        onChange={this.inputChangeHandler}
+                        type="text"
+                        placeholder="Last Name"
+                    />
+                    <button onClick={this.formSubmitHandler}>Submit</button>
+                </form>
+            </div>
+        );
+    }
+}
 
 export default SignUp;
