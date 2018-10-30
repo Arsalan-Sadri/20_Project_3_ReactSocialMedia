c047cee30a6d78e3726afb372760df8d2eedf372
Arsalan Sadri <asadri62@gmail.com>
2018-10-30 05:39:57 -0700

    updating the profile page with user info upon loading

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -16,20 +16,28 @@ class Profile extends React.Component {
             headers: {
                 Authorization: "Bearer " + localStorage.getItem("token")
             }
-        }).then(res => {
-            console.log(res);
-        });
+        }).then(res =>
+            this.setState({
+                firstName: res.data.firstName,
+                lastName: res.data.lastName,
+                email: res.data.email
+            })
+        );
     }
 
     render() {
         return (
             <div className="card">
-                <img className="card-img-top" src="#" alt="Card image" />
+                <img
+                    className="card-img-top"
+                    src="./avatar.png"
+                    alt="Card image"
+                />
                 <div className="card-body">
-                    <h4 className="card-title">(John Doe)</h4>
-                    <p className="card-text">
-                        email
-                    </p>
+                    <h4 className="card-title">
+                        {this.state.firstName} {this.state.lastName}
+                    </h4>
+                    <p className="card-text">{this.state.email}</p>
                     <a href="#" className="btn btn-info">
                         Update profile
                     </a>
