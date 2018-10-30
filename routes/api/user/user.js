eb408e1637c5ea818fcb71bbf0764761d8395c60
Arsalan Sadri <asadri62@gmail.com>
2018-10-30 00:58:57 -0700

    minor changes

--- routes/api/user/user.js
+++ routes/api/user/user.js
@@ -3,19 +3,24 @@ const path = require("path");
 const router = require("express").Router();
 const userControlller = require("../../../controllers/userController");
 
-router
+/* 
+        /api/user
+ */
+
+ router
     .route("/sign-up")
     .post(userControlller.createAndSignIn);
+
 
 router
     .route("/sign-in")
     .post(userControlller.signIn);
     
-// Matches with "/api/user/:username"
+
 router
     .route("/:username")
     .get((req, res) => {
-        // token is in req headers
+        // token is in the req headers
         res.send("Hiiii");
     });
 
