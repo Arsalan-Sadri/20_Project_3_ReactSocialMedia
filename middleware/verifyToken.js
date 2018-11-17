4e305ed7b7b2b5bdfa1f8f669f4f36bf21493a29
Arsalan Sadri <asadri62@gmail.com>
2018-11-16 18:09:44 -0800

    small changes

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -4,11 +4,15 @@ module.exports = (req, res, next) => {
     if (req.headers.authorization) {
         const token = req.headers.authorization.split(" ")[1];
         jwt.verify(token, process.env.JWT_KEY, function(err, decoded) {
-            if (err) res.send("2Access denied! Please sign in!");
+            if (err) res.send("Access denied! Please sign in!");
             else {
-                console.log("\n**********\nUser authenticated!\n**********\n");
+                console.log(
+                    `\n**********\nUser authenticated:\n< ${
+                        decoded.email
+                    } >\n**********\n`
+                );
                 next();
             }
         });
-    } else res.send("1Access denied! Please sign in!");
+    } else res.send("Access denied! Please sign in!");
 };
