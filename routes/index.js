5e435354613df1cc38a2e3b10fe3ff9c904cf61e
Arsalan Sadri <asadri62@gmail.com>
2018-11-16 19:00:47 -0800

    Add more routes to index.js under routes folder to make all routed protected except landing page

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,23 @@
 const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
+const middleware = require("../middleware");
 
-router.use("/api", apiRoutes);
+router
+    .route("/")
+    .get((req, res) =>
+        res.sendFile(path.join(__dirname, "../client/build/index.html"))
+    );
 
-router.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+router
+    .use("/api", apiRoutes);
+
+router
+    .use(middleware.verifyToken);
+
+router
+    .use((req, res) =>
+        res.sendFile(path.join(__dirname, "../client/build/index.html"))
+);
 
 module.exports = router;
