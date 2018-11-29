1ebcae30f4b21e980645b06618acb14c896a0d46
Arsalan Sadri <asadri62@gmail.com>
2018-11-28 19:20:36 -0800

    Reverting back to separate forms

--- client/src/pages/ProfilePg/ProfilePg.js
+++ client/src/pages/ProfilePg/ProfilePg.js
@@ -4,61 +4,14 @@ import Navbar from "../../components/Navbar";
 import ProfileCard from "../../components/ProfileCard";
 import Footer from "../../components/Footer";
 import PhotoCarousel from "../../components/PhotoCarousel";
-import SignUpPg from "../SignUpPg";
-import SignUpForm from "../../components/SignUpForm";
-import API from "../../utils/API";
+import SignUpPg from "../../pages/SignUpPg";
 
 class ProfilePg extends React.Component {
-    state = {
-        updateFormIsShown: false,
-        firstName: "",
-        lastName: "",
-        jobTitle: "",
-        city: "",
-        state: "",
-        username: "",
-        email: ""
-    };
-
     componentDidMount() {
         setTimeout(() => {
             localStorage.clear();
         }, 600000);
     }
-
-    updateProfileBtnHandler = () => {
-        this.setState({
-            updateFormIsShown: true
-        });
-
-        /*  Make api call to get user's info and 
-            have them displayed as placeholders in the form
-            then, update the state
-        */
-        API.getDbUser(localStorage.getItem("username")).then(res =>
-            this.setState({
-                firstName: res.data.firstName,
-                lastName: res.data.lastName,
-                jobTitle: res.data.jobTitle,
-                city: res.data.city,
-                state: res.data.state,
-                username: res.data.username,
-                email: res.data.email,
-                photoURL: res.data.photoURL
-            })
-        );
-    };
-
-    updateBtnHandler = userNewInfo => {
-        // Code to handle submit button to update user info in DB
-        console.log(userNewInfo);
-        // API.updateUser(userNewInfo)
-        //     .then(res => {
-        //         localStorage.setItem("username", res.data.username);
-        //         window.location.pathname = "/profile/" + localStorage.getItem("username");
-        //     })
-        //     .catch(err => console.log(err));
-    };
 
     render() {
         if (!localStorage.getItem("token")) return <SignUpPg />;
@@ -68,29 +21,10 @@ class ProfilePg extends React.Component {
                 <div className="container-fluid bg-light py-5">
                     <div className="row">
                         <div className="col-md-4">
-                            <ProfileCard
-                                updateProfileBtnHandler={this.updateProfileBtnHandler}
-                            />
+                            <ProfileCard />
                         </div>
-                        <div className="col-md-2" />
-                        <div className="col-md-6">
-                            {this.state.updateFormIsShown ? (
-                                <SignUpForm
-                                    btnValue="Update"
-                                    formBtnHandler={this.updateBtnHandler}
-                                    firstName={this.state.firstName}
-                                    lastName={this.state.lastName}
-                                    jobTitle={this.state.jobTitle}
-                                    city={this.state.city}
-                                    state={this.state.state}
-                                    username={this.state.username}
-                                    email={this.state.email}
-                                    password="********"
-                                    confirmPass="********"
-                                />
-                            ) : (
-                                <PhotoCarousel />
-                            )}
+                        <div className="col-md-8">
+                            <PhotoCarousel />
                         </div>
                     </div>
                 </div>
--- client/src/pages/SignUpPg/SignUpPg.js
+++ client/src/pages/SignUpPg/SignUpPg.js
@@ -3,20 +3,8 @@ import "./SignUpPg.css";
 import NavbarLanPg from "../../components/NavbarLanPg";
 import SignUpForm from "../../components/SignUpForm";
 import Footer from "../../components/Footer";
-import API from "../../utils/API";
 
 class SignUpPg extends React.Component {
-    signUpBtnHandler = userInfo => {
-        API.signUp(userInfo)
-            .then(res => {
-                localStorage.clear();
-                localStorage.setItem("token", res.data.token);
-                localStorage.setItem("username", res.data.username);
-                window.location.pathname = "/profile/" + localStorage.getItem("username");
-            })
-            .catch(err => console.log(err));
-    };
-
     render() {
         return (
             <React.Fragment>
@@ -25,19 +13,7 @@ class SignUpPg extends React.Component {
                     <div className="row">
                         <div className="col-md-5">
                             <section>
-                                <SignUpForm
-                                    firstName="First name"
-                                    lastName="Last name"
-                                    jobTitle="Job title"
-                                    city="City"
-                                    state="State"
-                                    username="Username"
-                                    email="Email"
-                                    password="Password"
-                                    confirmPass="Confirm password"
-                                    btnValue="Sign up"
-                                    formBtnHandler={this.signUpBtnHandler}
-                                />
+                                <SignUpForm />
                             </section>
                         </div>
                         <div className="col-md-7">
