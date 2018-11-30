3a19ef91d52094a8378b3c8bff8ca6111352c0db
Arsalan Sadri <asadri62@gmail.com>
2018-11-29 23:50:38 -0800

    Small changes

--- models/user/user.js
+++ models/user/user.js
@@ -9,28 +9,28 @@ const userSchema = new mongoose.Schema({
     lastName: {
         type: String,
         trim: true,
-        required: "Last name is required"
+        required: "Last name is required!"
     },
     username: {
         type: String,
         trim: true,
-        required: "Username is required",
+        required: "Username is required!",
         unique: true
     },
     email: {
         type: String,
         trim: true,
-        required: "Email address is required",
+        required: "Email address is required!",
         unique: true,
         match: [
             /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/,
-            "Please enter a valid e-mail address"
+            "Please enter a valid e-mail address!"
         ]
     },
     password: {
         type: String,
         trim: true,
-        required: "Password is required",
+        required: "Password is required!",
         validate: [
             function(input) {
                 return input.length >= 8;
