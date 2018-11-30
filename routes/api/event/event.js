7da3017b8749ebdf7da6da63a8c4822a5e73bc14
Arsalan Sadri <asadri62@gmail.com>
2018-11-30 00:10:49 -0800

    Adding an event routes

---
+++ routes/api/event/event.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const userControlller = require("../../../controllers/eventController");
+const middleware = require("../../../middleware");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, "./uploads/"),
+    filename: (req, file, cb) =>
+        cb(null, new Date().toISOString() + "-" + file.originalname)
+});
+const upload = multer({ storage });
+
+/* 
+        /api/event
+ */
+
+router
+    .use(middleware.verifyToken);
+
+ router
+    .route("/create-event")
+    .post(upload.single("photo"), eventController.createEvent);
+
+module.exports = router;
