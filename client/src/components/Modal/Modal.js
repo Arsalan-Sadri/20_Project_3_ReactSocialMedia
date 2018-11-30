6011b7a6ad433876ae3a708781baef99cedea7c5
Arsalan Sadri <asadri62@gmail.com>
2018-11-29 17:43:33 -0800

    Adding a modal component

---
+++ client/src/components/Modal/Modal.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactModal from "react-modal";
+import "./Modal.css";
+
+ReactModal.setAppElement("#root");
+
+const Modal = props => (
+    <ReactModal
+        className={props.modalIsClose ? "modal-open" : "modal-close"}
+        overlayClassName="my-overlay"
+        isOpen={props.modalIsOpen}>
+        {/* Partial Bootstrap modal */}
+        <div className="modal-dialog m-0" role="document">
+            <div className="modal-content">
+                <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalLabel">
+                        Modal header
+                    </h5>
+                    <button
+                        className="close"
+                        onClick={props.closeModal}
+                        aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div className="modal-body">Modal body</div>
+                <div className="modal-footer">
+                    <button className="btn btn-primary" onClick={props.closeModal}>
+                        Close
+                    </button>
+                </div>
+            </div>
+        </div>
+    </ReactModal>
+);
+
+export default Modal;
