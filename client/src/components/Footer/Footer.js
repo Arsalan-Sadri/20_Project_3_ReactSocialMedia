449e2b2cc549555449d6bfbb000a85193fc30d42
Arsalan Sadri <asadri62@gmail.com>
2018-12-02 23:15:29 -0800

    Adding css to footer. Making it stick at the bottom, adding background color

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => (
-    <div className="text-secondary text-center my-footer">
+    <div className="text-center py-2 my-footer">
         <a className="pr-1">Contact |</a>
         <a className="pr-1">Terms of Use |</a>
         <a className="pr-1">Privacy Policy |</a>
