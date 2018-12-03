4d0d59707c443fe98d07bf50bbce0aeb9863667f
Arsalan Sadri <asadri62@gmail.com>
2018-12-02 17:19:30 -0800

    Adding skeleton for no event component

---
+++ client/src/components/NoEvent/NoEvent.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./NoEvent.css";
+
+const NoEvent = () => (
+    <div className="no-event-div">
+        Oops! No event has been associated with this account!
+    </div>
+);
+
+export default NoEvent;
--- client/src/pages/EventsPg/EventsPg.js
+++ client/src/pages/EventsPg/EventsPg.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../../components/Navbar";
 import EventCard from "../../components/EventCard";
 import Footer from "../../components/Footer";
+import NoEvent from "../../components/NoEvent";
 import "./EventsPg.css";
 import API from "../../utils/api/API";
 
@@ -27,23 +28,27 @@ class EventsPg extends React.Component {
                 <div className="container-fluid bg-light py-5">
                     <div className="row mb-5">
                         <div className="col-md">
-                            {this.state.events.map(event => (
-                                <EventCard
-                                    key={event._id}
-                                    name={event.name}
-                                    category={event.category}
-                                    capacity={event.capacity}
-                                    date={event.data}
-                                    time={event.time}
-                                    city={event.city}
-                                    state={event.state}
-                                    zipCode={event.zipCode}
-                                    description={event.description}
-                                    photoURL={`http://${window.location.host}/${
-                                        event.photoURL
-                                    }`}
-                                />
-                            ))}
+                            {this.state.events.length === 0 ? (
+                                <NoEvent />
+                            ) : (
+                                this.state.events.map(event => (
+                                    <EventCard
+                                        key={event._id}
+                                        name={event.name}
+                                        category={event.category}
+                                        capacity={event.capacity}
+                                        date={event.data}
+                                        time={event.time}
+                                        city={event.city}
+                                        state={event.state}
+                                        zipCode={event.zipCode}
+                                        description={event.description}
+                                        photoURL={`http://${window.location.host}/${
+                                            event.photoURL
+                                        }`}
+                                    />
+                                ))
+                            )}
                         </div>
                     </div>
                 </div>
