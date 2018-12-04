a62a772102f62643ae1e06df5dfe327acc44cdaa
Arsalan Sadri <asadri62@gmail.com>
2018-12-04 13:43:27 -0800

    Adding columns to event card

--- client/src/components/EventCard/EventCard.js
+++ client/src/components/EventCard/EventCard.js
@@ -8,31 +8,44 @@ class EventCard extends React.Component {
                 <img className="card-img-top" src={this.props.photoURL} alt="card" />
                 <div className="card-body">
                     <h4 className="card-title">{this.props.name}</h4>
-                    <p className="my-0">
-                        <i className="far fa-calendar-alt" /> {this.props.date}
-                        {", "}
-                        <i className="far fa-clock" /> {this.props.time}
-                    </p>
-                    <p className="my-0">
-                        <i className="fas fa-male" /> {this.props.capacity}
-                    </p>
-                    <p className="my-0">
-                        <i className="fas fa-map-marker-alt" /> {this.props.street}
-                        <br />
-                        {"   "}
-                        {this.props.city}
-                        {","} {this.props.state} {this.props.zipCode}
-                    </p>
-                    <p className="my-0">
-                        <i className="fas fa-info-circle text-info" />{" "}
-                        {this.props.description}
-                    </p>
+                    <div className="row">
+                        <div className="col-auto pr-0">
+                            <i className="far fa-calendar-alt text-warning" />
+                        </div>
+                        <div className="col-auto pl-2 pr-4">
+                            <p className="my-0">{this.props.date}</p>
+                        </div>
+                        <div className="col-auto px-0">
+                            <i className="far fa-clock text-danger" />
+                        </div>
+                        <div className="col-auto pl-1">{this.props.time}</div>
+                    </div>
+                    <div className="row">
+                        <div className="col-auto pr-0">
+                            <i className="fas fa-male pl-1 text-secondary" />
+                            <br />
+                            <i className="fas fa-map-marker-alt text-primary" />
+                            <br />
+                            <br />
+                            <i className="fas fa-info-circle text-info" />
+                        </div>
+                        <div className="col-auto pl-2">
+                            <p className="my-0">{this.props.capacity}</p>
+                            <p className="my-0">
+                                {this.props.street}
+                                <br />
+                                {this.props.city}
+                                {","} {this.props.state} {this.props.zipCode}
+                            </p>
+                            <p className="my-0"> {this.props.description}</p>
+                        </div>
+                    </div>
                 </div>
                 <div className="card-footer">
-                    <a className="btn my-2 ml-1 mr-2 event-card" href="/modify-event">
+                    <a className="btn my-2 ml-1 mr-2 event-card-btn" href="/modify-event">
                         <i className="far fa-edit" /> Modify event
                     </a>
-                    <a className="btn ml event-card" href="/delete-event">
+                    <a className="btn ml event-card-btn" href="/delete-event">
                         <i className="far fa-calendar-times" /> Delete event
                     </a>
                 </div>
