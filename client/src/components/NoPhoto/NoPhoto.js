569a370a390fee8395f845a747a68f4ca7f6d4d4
Arsalan Sadri <asadri62@gmail.com>
2018-12-04 22:01:48 -0800

    Added NoPhoto component to photos page

--- client/src/components/NoPhoto/NoPhoto.js
+++ client/src/components/NoPhoto/NoPhoto.js
@@ -5,8 +5,8 @@ const NoEvent = () => (
     <div className="no-photo-div">
         <div className="text-danger font-weight-bold">Oops!</div>
         <div className="font-italic">You do not have any photos!</div>
-        <a className="btn my-2" href="/upload-photos">
-            <i className="fas fa-cloud-upload-alt" aria-hidden="true" /> Upload photos
+        <a className="btn add-photos-link" href="/upload-photos">
+            Add photos
         </a>
     </div>
 );
--- client/src/pages/PhotosPg/PhotosPg.js
+++ client/src/pages/PhotosPg/PhotosPg.js
@@ -1,12 +1,16 @@
 import React from "react";
-// import API from "../../utils/api/API";
 import Navbar from "../../components/Navbar";
-// import PreviewCard from "../../components/PreviewCard";
+import NoPhoto from "../../components/NoPhoto";
 import Footer from "../../components/Footer";
 import SignUpPg from "../SignUpPg";
 import "./PhotosPg.css";
+// import API from "../../utils/api/API";
 
 class PhotosPg extends React.Component {
+    state = {
+        renderNoPhoto: true
+    };
+
     componentDidMount() {
         setTimeout(() => {
             localStorage.clear();
@@ -23,7 +27,9 @@ class PhotosPg extends React.Component {
             <React.Fragment>
                 <Navbar />
                 <div className="container-fluid bg-light py-5 main-container">
-                    <div className="row" />
+                    <div className="row">
+                        {this.state.renderNoPhoto ? <NoPhoto /> : 1}
+                    </div>
                 </div>
                 <Footer />
             </React.Fragment>
