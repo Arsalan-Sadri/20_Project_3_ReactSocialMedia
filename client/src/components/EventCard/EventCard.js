db952023c7f5cc232295b3315d8e5a6cece6820c
Arsalan Sadri <asadri62@gmail.com>
2018-12-05 19:08:55 -0800

    Removing margin from event card

--- client/src/components/EventCard/EventCard.js
+++ client/src/components/EventCard/EventCard.js
@@ -4,21 +4,23 @@ import "./EventCard.css";
 class EventCard extends React.Component {
     render() {
         return (
-            <div className="card event-card m-5">
+            <div className="card event-card">
                 <img className="card-img-top" src={this.props.photoURL} alt="card" />
                 <div className="card-body">
                     <h4 className="card-title">{this.props.name}</h4>
                     <div className="row">
                         <div className="col-auto pr-0">
-                            <i className="far fa-calendar-alt text-warning" />
-                        </div>
-                        <div className="col-auto pl-2 pr-4">
-                            <p className="my-0">{this.props.date}</p>
+                            <p className="my-0">
+                                <i className="far fa-calendar-alt text-warning" />{" "}
+                                {this.props.date}
+                            </p>
                         </div>
                         <div className="col-auto px-0">
-                            <i className="far fa-clock text-danger" />
+                            <p className="my-0">
+                                <i className="far fa-clock text-danger" />{" "}
+                                {this.props.time}{" "}
+                            </p>
                         </div>
-                        <div className="col-auto pl-1">{this.props.time}</div>
                     </div>
                     <div className="row">
                         <div className="col-auto pr-0">
@@ -30,7 +32,7 @@ class EventCard extends React.Component {
                             <i className="fas fa-info-circle text-info" />
                         </div>
                         <div className="col pl-2">
-                            <p className="my-0">{this.props.capacity}</p>
+                            <p className="my-0">{this.props.capacity+" going"}</p>
                             <p className="my-0">
                                 {this.props.street}
                                 <br />
@@ -42,10 +44,10 @@ class EventCard extends React.Component {
                     </div>
                 </div>
                 <div className="card-footer">
-                    <a className="btn my-2 ml-1 mr-2 event-card-btn" href="/modify-event">
+                    <a className="btn my-2 mr-2 event-card" href="/modify-event">
                         <i className="far fa-edit" /> Modify event
                     </a>
-                    <a className="btn ml event-card-btn" href="/delete-event">
+                    <a className="btn event-card" href="/delete-event">
                         <i className="far fa-calendar-times" /> Delete event
                     </a>
                 </div>
