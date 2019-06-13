4017c00317c6f7cf355fd38c8b1340decf1a0e84
Arsalan Sadri <asadri62@gmail.com>
2019-06-12 23:20:54 -0700

    Reverted back to the original .gitignore on master branch. Instead will create a separate branch for heroku deployment

---
+++ client/build/precache-manifest.6438af04037aef021d51b3aa285f408e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71e8d9eadf2461127411a477ae673598",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c5ba7c50f6bc784a20e",
+    "url": "/static/css/main.06f32f56.chunk.css"
+  },
+  {
+    "revision": "05654c0f64579b519f54",
+    "url": "/static/js/2.34f6a925.chunk.js"
+  },
+  {
+    "revision": "3c5ba7c50f6bc784a20e",
+    "url": "/static/js/main.e0933b27.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
