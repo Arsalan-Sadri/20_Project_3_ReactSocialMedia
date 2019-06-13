01f3295b339c1483162ee70a863289d5105ed67f
Arsalan Sadri <asadri62@gmail.com>
2019-06-12 22:09:11 -0700

    Added href att. to Footer.js

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,10 +3,10 @@ import "./Footer.css";
 
 const Footer = () => (
     <div className="text-center py-2 my-footer">
-        <a className="pr-1">Contact |</a>
-        <a className="pr-1">Terms of Use |</a>
-        <a className="pr-1">Privacy Policy |</a>
-        <a className="pr-1">About</a>
+        <a href="#" className="pr-1">Contact |</a>
+        <a href="#" className="pr-1">Terms of Use |</a>
+        <a href="#" className="pr-1">Privacy Policy |</a>
+        <a href="#" className="pr-1">About</a>
     </div>
 );
 
